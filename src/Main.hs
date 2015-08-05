aca5fd0999fe8f94e9cfb988e560642f28cfd2b2
Nikita Karetnikov <nikita@karetnikov.org>
2015-08-05 19:22:58 +0300

    Add a simple server

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
+
 module Main where
 
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Char
+import Network.Wai.Handler.Warp
+import System.Environment
+import Web.Scotty
+
+import Bake
+
 main :: IO ()
-main = undefined
+main = do
+    let usage = error "Usage: PORT"
+    getArgs >>= \case
+        [port] -> if all isNumber port
+                  then handle $ read port
+                  else usage
+        _      -> usage
+
+handle :: Port -> IO ()
+handle port = scotty port $ do
+    get "" $ do
+        bs <- body
+        liftIO $ print (decode bs :: Maybe MergeRequest)
